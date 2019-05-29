99292fd83fcc07d07467ded6d62a427ddefd379d
Martin Stephenson <funkyspider@gmail.com>
2019-05-29 20:41:32 +0100

    create

---
+++ DXApplication1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace DXApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.SplineSeriesView splineSeriesView1 = new DevExpress.XtraCharts.SplineSeriesView();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(splineSeriesView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisX.WholeRange.Auto = false;
+            xyDiagram1.AxisX.WholeRange.MaxValueSerializable = "9";
+            xyDiagram1.AxisX.WholeRange.MinValueSerializable = "0";
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisY.WholeRange.Auto = false;
+            xyDiagram1.AxisY.WholeRange.MaxValueSerializable = "200";
+            xyDiagram1.AxisY.WholeRange.MinValueSerializable = "0";
+            this.chartControl1.Diagram = xyDiagram1;
+            this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl1.Legend.Name = "Default Legend";
+            this.chartControl1.Location = new System.Drawing.Point(0, 0);
+            this.chartControl1.Name = "chartControl1";
+            series1.Name = "Series 1";
+            series1.View = splineSeriesView1;
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1};
+            this.chartControl1.SeriesTemplate.SeriesColorizer = null;
+            this.chartControl1.Size = new System.Drawing.Size(737, 342);
+            this.chartControl1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 342);
+            this.Controls.Add(this.chartControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(splineSeriesView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DXApplication1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using DevExpress.XtraCharts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DXApplication1
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        private List<SeriesPoint> tempPoints;
+        DateTime _lastPlotTime = DateTime.Now;
+        int _val;
+        Random r = new Random();
+        DateTime AxisStartTime = DateTime.Now - new TimeSpan(0,0,1,0);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+
+        private void InitializeChart()
+        {
+            Series series = new Series();
+
+            // Adjust series
+            series.ArgumentScaleType = ScaleType.DateTime;
+            series.ValueScaleType = ScaleType.Numerical;
+            series.Label.Border.Visible = false;
+            series.Label.BackColor = Color.Transparent;
+            series.CrosshairLabelVisibility = DevExpress.Utils.DefaultBoolean.False;
+            series.ChangeView(ViewType.Area);
+            ((AreaSeriesView)series.View).Color = Color.Aqua;
+            ((AreaSeriesView)series.View).MarkerOptions.Kind = MarkerKind.Diamond;
+            ((AreaSeriesView)series.View).MarkerOptions.Size = 7;
+
+            series.Points.Add(new SeriesPoint(DateTime.Now, new double[] { 0 }));
+
+            chartControl1.Series.Add(series);
+
+            // Adjust time axis
+            ((XYDiagram)chartControl1.Diagram).AxisX.DateTimeScaleOptions.MeasureUnit = DateTimeMeasureUnit.Second;
+            ((XYDiagram)chartControl1.Diagram).AxisX.Label.DateTimeOptions.Format = DateTimeFormat.ShortTime;
+            ((XYDiagram)chartControl1.Diagram).AxisX.DateTimeScaleOptions.GridAlignment = DateTimeGridAlignment.Second;
+            ((XYDiagram)chartControl1.Diagram).AxisX.Range.SideMarginsEnabled = false;
+            ((XYDiagram)chartControl1.Diagram).AxisX.MinorCount = 10;
+            ((XYDiagram)chartControl1.Diagram).Margins.Right = 25;
+            //((XYDiagram)chartControl1.Diagram).AxisX.GridSpacingAuto = false;
+
+            // Adjust label
+            ChartTitle title = new ChartTitle();
+
+            title.Text = "Title";
+            chartControl1.Titles.Add(title);
+
+            tempPoints = new List<SeriesPoint>(60);
+            timer1.Enabled = true;
+        }
+
+        private void AdjustChartRange()
+        {
+            if (AxisStartTime < DateTime.Now - new TimeSpan(0, 0, 1, 0)) {
+                AxisStartTime = DateTime.Now - new TimeSpan(0, 0, 1, 0);
+            }
+
+            ((XYDiagram)chartControl1.Diagram).AxisX.Range.MinValue = AxisStartTime;
+            ((XYDiagram)chartControl1.Diagram).AxisX.Range.MaxValue = DateTime.Now ;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeChart();
+            //            Form1_Resize(null, null);
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            _lastPlotTime = DateTime.Now;
+
+            _val = r.Next(100, 200);
+
+            SeriesPoint nextPoint = new SeriesPoint(DateTime.Now, _val);
+
+            chartControl1.Titles[0].Text = $"Current value: {_val}";
+
+            foreach (SeriesPoint point in chartControl1.Series[0].Points)
+                if (Convert.ToDateTime(point.Argument) < DateTime.Now - new TimeSpan(0, 0, 0, 59))
+                    tempPoints.Add(point);
+
+            if (tempPoints.Count > 0)
+                tempPoints.RemoveAt(0);
+
+            foreach (SeriesPoint point in tempPoints)
+                chartControl1.Series[0].Points.Remove(point);
+
+            tempPoints.Clear();
+
+            chartControl1.Series[0].Points.Add(nextPoint);
+
+            AdjustChartRange();
+        }
+    }
+}
